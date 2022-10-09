91f6b636de8df52ab1ed28cf741c8c4bde90910f
Dev XO <code.devxo@gmail.com>
2022-10-09 09:13:25 +0200

    Chore: update comments
    Update to conventional Google comments.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,9 @@ module.exports = {
 		'cypress/globals': true,
 	},
 	plugins: ['cypress'],
-	// We're using vitest which has a very similar API to jest
-	// (so the linting plugins work nicely), but it means we have to explicitly
-	// set the jest version.
+
+	// We're using vitest which has a very similar API to jest,
+	// (so the linting plugins work nicely), but we have to explicitly set the jest version.
 	settings: {
 		jest: {
 			version: 28,
